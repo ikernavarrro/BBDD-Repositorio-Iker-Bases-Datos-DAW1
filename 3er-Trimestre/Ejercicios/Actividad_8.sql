f7e4824d0cd2373e768a2d23e3d23ce1aabda389
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2026-03-31 17:09:51 +0200

    Añadido enunciado Actividad 8

---
+++ 3er-Trimestre/Ejercicios/Actividad_8.sql
@@ -0,0 +1,41 @@
+-- Actividad 8 --
+
+--1. Nombre de los trabajadores cuya tarifa este entre 10 y 12 euros.
+
+--2. ¿Cuáles son los oficios de los trabajadores asignados al edificio 435?
+
+--3. Indicar el nombre del trabajador y el de su supervisor.
+
+--4. Nombre de los trabajadores asignados a oficinas.
+
+--5. ¿Qué trabajadores reciben una tarifa por hora mayor que la de su supervisor?
+
+--6. ¿Cuál es el número total de días que se han dedicado a fontanería en el edificio 312?
+
+--7. ¿Cuántos tipos de oficios diferentes hay?
+
+--8. Para cada supervisor, ¿Cuál es la tarifa por hora más alta que se paga a un trabajador
+--que informa a ese supervisor?
+
+--Si queremos el nombre:
+
+--9. Para cada supervisor que supervisa a más de un trabajador, ¿cuál es la tarifa más alta
+--que se para a un trabajador que informa a ese supervisor?
+
+--10. Para cada tipo de edificio, ¿Cuál es el nivel de calidad medio de los edificios con
+--categoría 1? Considérense sólo aquellos tipos de edificios que tienen un nivel de calidad
+--máximo no mayor que 3.
+
+--11. ¿Qué trabajadores reciben una tarifa por hora menor que la del promedio?
+
+--12. ¿Qué trabajadores reciben una tarifa por hora menor que la del promedio de los
+--trabajadores que tienen su mismo oficio?
+
+--13. ¿Qué trabajadores reciben una tarifa por hora menor que la del promedio de los
+--trabajadores que dependen del mismo supervisor que él?
+
+--14. Seleccione el nombre de los electricistas asignados al edificio 435 y la fecha en la que
+--empezaron a trabajar en él.
+
+--15. ¿Qué supervisores tienen trabajadores que tienen una tarifa por hora por encima de
+--los 12 euros?
